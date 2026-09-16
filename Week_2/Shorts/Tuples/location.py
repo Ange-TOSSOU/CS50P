302de3530ce927bf60ae1ac196166f91b3e0e88e
Ange TOSSOU <angetossou.dev@gmail.com>
2026-09-16 17:50:49 +0200

    Week 2 - Loops : without problem set

---
+++ Week_2/Shorts/Tuples/location.py
@@ -0,0 +1,15 @@
+import sys
+
+def main():
+    coordinate_tuple = (42.376, -71.115)
+    coordinate_list = [42.376, -71.115]
+
+    latitude, longitude = coordinate_tuple
+    print(f"Latitude: {latitude}")
+    print(f"Longitude: {longitude}")
+
+    print(f"{sys.getsizeof(coordinate_tuple)} bytes")
+    print(f"{sys.getsizeof(coordinate_list)} bytes")
+
+
+main()
